565f69f8d86a3db4ecbd18adb2c55fbb3282f9a5
cauelima1 <cauezak@gmail.com>
2025-12-09 12:18:44 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
+
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    })->name('app.produtos');
+});
+
+
+Route::get('/rota1', function () {
+    echo "Rota 1";
+})->name('site.rota1');
+
+
+Route::get('/rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2', '/rota1');
+
+
+Route::get(
+    '/contato/{nome}/{categoria_id}',
+    function (
+        string $nome,
+        int $categoria_id = 1
+    ) {
+        echo "Estamos aqui: $nome - $categoria_id";
+    }
+)
+    ->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
